d37ee9fc0bac0d135dd33508a295308d983a6959
Bhaskar Rajbongshi <bhaskar.rajbongshi@verizonmedia.com>
2021-01-05 18:46:48 -0800

    setup tabs and downloaded pods

--- Podfile
+++ Podfile
@@ -2,4 +2,15 @@
 
 target 'Tiktok' do
   use_frameworks!
+
+  pod 'Firebase/Core'
+  pod 'Firebase/Storage'
+  pod 'Firebase/Database'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Analytics'
+  pod 'Firebase/Crashlytics'
+
+  pod 'Appirater'
+  pod 'SDWebImage'
+
 end
--- Tiktok/Resources/AppDelegate.swift
+++ Tiktok/Resources/AppDelegate.swift
@@ -6,18 +6,22 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+ 
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = HomeViewController()
+        window?.rootViewController = TabBarViewController()
         
         window?.makeKeyAndVisible()
+        
+        FirebaseApp.configure()
+        
         return true
     }
 
